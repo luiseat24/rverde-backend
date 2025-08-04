3fcbeb5bfb3de3f9ba31b6433a311aae4b1a8c68
LUIS ALVARADO <130623453+luiseat24@users.noreply.github.com>
2025-08-04 10:03:11 -0400

    primer commit

---
+++ src/main/java/com/example/rverde/RverdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RverdeApplication.class, args);
+	}
+
+}
